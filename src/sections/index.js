37c9e3c167f91ae9a644ac894dbe378a05299a7e
nitish95087 <nitishpandit95087546@gmail.com>
2023-09-22 12:08:15 +0530

    nike app first version

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {Hero, PopularProducts, SuperQuality, Services, CustomerReviews, SpecialOffers, Subscribe, Footer};
